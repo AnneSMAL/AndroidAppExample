f52458a801a76be3be03df1840676b62e1a4ac0f
lisatr2812 <lisa.trapletti@gmail.com>
2020-10-16 16:53:01 +0200

    Module 5 - Presentation screen and strings (wip)

---
+++ app/src/main/java/be/henallux/ig3/android/app/example/ui/module5/AsyncPresentationFragment.java
@@ -0,0 +1,21 @@
+package be.henallux.ig3.android.app.example.ui.module5;
+
+import be.henallux.ig3.android.app.example.R;
+import be.henallux.ig3.android.app.example.ui.ModulePresentationFragment;
+
+public class AsyncPresentationFragment extends ModulePresentationFragment {
+    @Override
+    protected Integer getTitleResources() {
+        return R.string.module_5_title;
+    }
+
+    @Override
+    protected Integer getDescriptionResources() {
+        return R.string.module_5_title;
+    }
+
+    @Override
+    protected Integer getNextScreenNavigationAction() {
+        return null;
+    }
+}
