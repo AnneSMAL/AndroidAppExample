76c75799a48308c62024832499f7d4c6cd7e77a2
lisatr2812 <lisa.trapletti@gmail.com>
2020-10-17 18:10:27 +0200

    Module 5 - Documentation

--- app/src/main/java/be/henallux/ig3/android/app/example/repositories/web/PizzaWebService.java
+++ app/src/main/java/be/henallux/ig3/android/app/example/repositories/web/PizzaWebService.java
@@ -6,7 +6,19 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 /**
- * Java Interface, exposing your different endpoints
+ * Java Interface, exposing your different endpoints. </br>
+ * If you would like to mock your backend server to test your application, you can use one of those
+ * different tools : </br>
+ * - Mockoon; </br>
+ * - Beeceptor; </br>
+ * - ...; </br>
+ *
+ * If you would like to visualize the content of your web API, you can also use a
+ * REST API Client Interface. It can be really useful for you to check the structure of JSON
+ * sent / needed by your API. You can use one of those : </br>
+ * - Postman; </br>
+ * - Insomnia; </br>
+ * - ...;
  */
 public interface PizzaWebService {
 
